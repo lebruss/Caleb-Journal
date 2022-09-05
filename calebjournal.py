decf7f5fd6fcdf6710f123a63e9f6836efd6869a
Caleb James Russell <69431631+lebruss@users.noreply.github.com>
2022-09-06 00:36:50 +0300

    Add files via upload
    File name of the journal entry is now correct. It is a timestamp + .txt .
    The user input command E or e now gets removed from the actual .txt of the journal.
    I made the code more handsome, commented some sensible things, and gave the variables descriptive names.
    Unless I think of some more features, I will leave this one alone for a while.

--- calebjournal.py
+++ calebjournal.py
@@ -1,14 +1,14 @@
 #caleb journal
 from datetime import datetime#I use this to get the current time. Then I put this in the journal entry's file name
-now = datetime.now()
-filename = now.strftime("%d/%m%Y %H%M%S")
-filename.replace(':', '')#remove : character from "now", because : ultimately cannot be used in Windows filenames
-filename.replace('/', '')#remove : character from "now", because : ultimately cannot be used in Windows filenames
-print(str(filename))
-filename = "today"#Change this to get the filename correct with today's date and time
-def save():#Save journal entry as a .txt file; Filename is current date and time; Filedirectory is the same as this python file
-    f = open(filename + ".txt", "w")
-    for line in line_list:#For each "line" in the list of journal lines provided by the user,
+now = str(datetime.now())
+now = now.replace(':', '')#remove : character from "now", because : ultimately cannot be used in Windows filenames
+now = now.replace('/', '')
+print("File name: " + now)
+def SaveAndExit():#Save journal entry as a .txt file; Filename is current date and time; Filedirectory is the same as this python file
+    f = open(now + ".txt", "w")#Open a .txt file: Today's date-time ("now" variable) .txt ; option "w" creates and opens this filename
+    for line in journal:#For each "line" in the list of journal lines provided by the user,
+        if line == "e" or line == "E":
+            del line#E or e is intended as the user command to Save and Exit; remove these from the actual journal
         f.write(line)#write each line to this file
     f.close()
 
@@ -16,16 +16,11 @@ def save():#Save journal entry as a .txt file; Filename is current date and time
 print("Caleb Journal--")
 print("E for exit-----\n")
 
-line_list = []#a list, which contains each string line of the user's input
+journal = []#a list, which contains each string line of the user's input
 while True:
-    line = str(input())
-    line_list.append(line)
-    line_list.append("\n")
-    if line == "e":
-        #for line in line_list:
-            #print(line)
-        save()
-        break
-    if line == "E":
-        save()
+    line = str(input())#Each journal "line" is a user input, separated by Enter key
+    journal.append(line)#Upon pressing Enter, add the user's journal "line" to the journal
+    journal.append("\n")#Adds new line to each input
+    if line == "e" or line == "E":#If user input is E or e , save and exit Calebjournal application
+        SaveAndExit()
         break
